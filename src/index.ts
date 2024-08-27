c3b73f10b4b42d9360200aa746bdb3a4cee78f8c
PupNicky <sailgamer03@gmail.com>
2024-08-27 21:49:06 +0200

    Init ts project first files

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Client } from "discord.js";
+import { config as dotenv } from "dotenv";
+
+// Load environment variables from .env file
+dotenv();
+
+// Create a new Discord client
+const bot = new Client({
+  intents: [
+    "Guilds",
+    "GuildMembers",
+    "GuildModeration",
+    "GuildVoiceStates",
+    "GuildMessages",
+    "GuildMessageReactions",
+    "DirectMessages",
+    "GuildInvites",
+    "MessageContent",
+  ],
+});
+
+// Event triggered when the bot is ready
+bot.on("ready", () => {
+  console.log(`Logged in as ${bot.user?.tag}!`);
+});
+
+export default bot;
+
+// Log in to Discord using the bot token from the .env file
+bot.login(process.env.BOT_TOKEN);
